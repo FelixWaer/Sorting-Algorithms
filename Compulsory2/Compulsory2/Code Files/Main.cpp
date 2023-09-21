5efda83a576cbeca0a0fe57882a6e4697cce5338
FelixWaer <felixwaer@gmail.com>
2023-09-21 17:56:12 +0200

    Updating

--- Compulsory2/Compulsory2/Code Files/Main.cpp
+++ Compulsory2/Compulsory2/Code Files/Main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 #define PRINT(X) std::cout << (X) << std::endl
 #define CLEAR_CONSOLE std::cout << "\033c"
+#define TIME(x) x = std::chrono::high_resolution_clock::now()
+#define PRINT_TIME_MS(text, start, end) std::cout << text << std::chrono::duration_cast<std::chrono::nanoseconds>((end) - (start)).count() << " ns" << std::endl
+
+class Timer
+{
+public:
+	Timer()
+	{
+	
+	}
+
+	~Timer()
+	{
+	
+	}
+private:
+	
+};
 
 void swap_Elements(int& elementA, int& elementB)
 {
@@ -10,6 +29,7 @@ void swap_Elements(int& elementA, int& elementB)
 	elementA = elementB;
 	elementB = tempNumber;
 }
+
 
 int partition_Array(int array[], int start, int end)
 {
@@ -41,6 +61,36 @@ void quickSorting(int array[], int start, int end)
 	quickSorting(array, partitionIndex + 1, end);
 }
 
+int partition_Array(std::vector<int>& vector, int start, int end)
+{
+	int const pivot = vector[end];
+	int leftElementIndex = start;
+
+	for (int i = start; i <= end; i++)
+	{
+		if (vector[i] < pivot)
+		{
+			swap_Elements(vector[i], vector[leftElementIndex]);
+			leftElementIndex++;
+		}
+	}
+	swap_Elements(vector[leftElementIndex], vector[end]);
+
+	return leftElementIndex;
+}
+
+void quickSorting(std::vector<int>& vector, int start, int end)
+{
+	if (start > end)
+	{
+		return;
+	}
+
+	int const partitionIndex = partition_Array(vector, start, end);
+	quickSorting(vector, start, partitionIndex - 1);
+	quickSorting(vector, partitionIndex + 1, end);
+}
+
 void print_Array(int array[], int arraySize)
 {
 	for (int i = 0; i < arraySize; i++)
@@ -50,23 +100,57 @@ void print_Array(int array[], int arraySize)
 	PRINT(" ");
 }
 
-int main() {
-	int arraySize = 10;
-	int theArray[10];
+void splitting_Array(int array[], int start, int mid, int end)
+{
+	int subArrayOneSize = mid - start + 1;
+	int subArrayTwoSize = end - mid;
 
-	int b = 0;
+	int* subArrayOne = new int[subArrayOneSize];
+	int* subArrayTwo = new int[subArrayTwoSize];
+
+	for (int i = 0; i < subArrayOneSize; i++)
+	{
+		subArrayOne[i] = array[start + i];
+	}
+	for (int i = 0; i < subArrayTwoSize; i++)
+	{
+		subArrayTwo[i] = array[mid + 1 + i];
+	}
+
+
+	delete[] subArrayOne;
+	delete[] subArrayTwo;
+
+}
+void mergeSort(int array[], int start, int end)
+{
+	if (start >= end)
+	{
+		return;
+	}
+
+	int const mid = start + (end - start) / 2;
+
+	mergeSort(array, start, mid);
+	mergeSort(array, mid + 1, end);
+	splitting_Array(array, start, mid, end);
+}
+
+int main() {
+	std::vector<int> vectorThatIsGonnaBeSorted;
+
+	int arraySizeChoice = 0;
 	while (true)
 	{
 		std::cin >> std::ws;
 		int c = std::cin.peek();
-		if (std::isdigit(c) || c=='-')
+		if (std::isdigit(c))
 		{
-			if (b == arraySize)
+			std::cin >> arraySizeChoice;
+			for (size_t i = 0; i < arraySizeChoice; i++)
 			{
-				break;
+				
 			}
-			std::cin >> theArray[b];
-			b++;
 			CLEAR_CONSOLE;
 		}
 		else
@@ -77,7 +161,10 @@ int main() {
 
 	print_Array(theArray, arraySize);
 
+	TIME(auto startTime);
 	quickSorting(theArray, 0, arraySize-1);
+	TIME(auto endTime);
+	PRINT_TIME_MS("time to sort array: ", startTime, endTime);
 
 	print_Array(theArray, arraySize);
 
